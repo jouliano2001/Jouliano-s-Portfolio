1d78271673ad1cc062ffc3f51b4194bb8be9423e
jouliano adel <Joulianoadel@gmail.com>
2025-06-08 19:39:15 +0300

    Updated Portfolio

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-
+import JoulianoLogo from '../../src/assets/JA..2-01-removebg-preview.png'
 const Footer: React.FC = () => {
   return (
     <footer className="bg-white dark:bg-gray-900 w-[100vw]">
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-            <a href="https://flowbite.com/" className="flex items-center">
-                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 me-3" alt="FlowBite Logo" />
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-            </a>
+                <img src={JoulianoLogo} className="h-15 me-3" alt="Jouliano Logo" /> 
           </div>
           <div className="flex">
             <div>
--- src/components/UI/resizable-navbar.tsx
+++ src/components/UI/resizable-navbar.tsx
@@ -9,6 +9,7 @@ import {
 } from "motion/react";
 
 import React, { useRef, useState } from "react";
+import JoulianosLogo from '../../assets/JA_1_1.png'
 
 
 interface NavbarProps {
@@ -235,12 +236,11 @@ export const NavbarLogo = () => {
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
       <img
-        src="https://assets.aceternity.com/logo-dark.png"
+        src={JoulianosLogo}
         alt="logo"
-        width={30}
-        height={30}
+        width={90}
+        height={90}
       />
-      <span className="font-medium text-black dark:text-white">Startup</span>
     </a>
   );
 };
